479741ccc4db4f30604c076d29cb314979a571ff
JohannaUlveklint <johanna.ulveklint@gmail.com>
2023-01-18 10:27:29 +0100

    Cosmetic changes in streamlit and preparing for grid search

--- app/streamlit.py
+++ app/streamlit.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import pandas as pd
-import warnings
-import numpy as np
-import gensim
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from io import StringIO
 
 
@@ -48,7 +42,7 @@ with load_file:
 
         # To read file as string:
         string_data = stringio.read()
-        st.write("**You uploaded this file:**")
+        st.write("**You uploaded this file (showing the first 500 characters):**")
         st.write(f"{string_data[:500]}...")
 
         # Can be used wherever a "file-like" object is accepted:
@@ -58,9 +52,6 @@ with load_file:
         # Also solution for multiple uploads at https://docs.streamlit.io/library/api-reference/widgets/st.file_uploader
 
     
-
-
-
 with models:
     class_col, reg_col = st.columns(2)
 
@@ -78,25 +69,32 @@ with models:
         return "Model saved somewhere"
     
     model = get_model()
-    # prediction = model.predict([prepare(string_data)])  # Maybe not string_data, maybe not in a list
-    # prediction = CATEGORIES[int(prediction[0][0])]
+    # pred_class = model.predict([prepare(string_data)])  # Maybe not string_data, maybe not in a list
+    # pred_class = CATEGORIES[int(prediction[0][0])]
     pred_class = "female"
-    class_col.write(f"The classification model predicts that the distribution of applicants will be mostly \033[1m{pred_class}\033[0m.")
+
+    class_weights = ["electric", "construct", "repair"]
+    weights = ''
+    for i in (class_weights):
+        weights += "\n" + "- " + i
+
+    class_col.write(f"The classification model predicts that the distribution of applicants will be mostly **{pred_class}**.")
 
     # Regression model
     reg_col.subheader("Results from regression model")
     pred_reg = 0.44
-    reg_col.write(f"The regression model predicts that \033[1m{round((1 - pred_reg) * 100, 1)}% women\033[0m and \033[1m{pred_reg * 100}% men\033[0m will apply for this job.")
+    reg_col.write(f"The regression model predicts that **{round((1 - pred_reg) * 100, 1)}% women** and **{pred_reg * 100}% men** will apply for this job.")
 
 
 with interpretation:
     col_1, col_2, col_3 = st.columns(3)
     col_2.subheader("Interpretated result:")
     if pred_class == "female":
-        col_2.markdown(f"* The classification model predicts that the distribution will be mostly \033[1m{pred_class}\033[0m.")
-        col_2.markdown(f"* The regression model predicts that {round((1 - pred_reg) * 100, 1)} wommen will apply for this job.")
+        col_2.markdown(f"* The classification model predicts that the distribution will be mostly **{pred_class}**.")
+        col_2.markdown(f"* The regression model predicts that **{round((1 - pred_reg) * 100, 1)}% women** will apply for this job.")
         col_2.markdown(f"* Due to a low share of jobs that mostly women applies to, the numbers from the regression model is probably misleading.")
-        col_2.markdown(f"* More correct numbers would be that over 70% of the applicants will be women.")
+        col_2.markdown(f"* More correct numbers would be that over **70% of the applicants will be women**.")
     else:
-        col_2.markdown(f"* The models predicts that the applicants for this job will be mostly {pred_class}, "\
-        f"{round((1 - pred_reg) * 100, 1)}% women and {pred_reg * 100}% men.")
+        col_2.markdown(f"* The models predicts that the applicants for this job will be mostly **{pred_class}**, "\
+        f"**{round((1 - pred_reg) * 100, 1)}% women** and **{pred_reg * 100}% men**.")
+        col_2.markdown(f"* The model has make its prediction based on following words: {weights}")
